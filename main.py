6d0a6aa53a0db6fc8286fd2f4efab9107fcc556b
maheshwari <archit.maheshwari@payufin.com>
2023-12-17 17:29:00 +0530

    query with arguments

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
-from graphene import Schema, ObjectType, String
+from graphene import Schema, ObjectType, String, Int, Field
+
+
+class UserType(ObjectType):
+    id = Int()
+    name = String()
+    age = Int()
 
 
 class Query(ObjectType):
-    hello = String(name=String(default_value='world'))
+    user = Field(UserType, user_id=Int())
 
-    def resolve_hello(self, info, name):
-        return f"Hello {name}"
+    users = [
+        {"id": 1, "name": "Andy Doe", "age": 33},
+        {"id": 2, "name": "Adam Smith", "age": 34},
+        {"id": 3, "name": "Adam Smith", "age": 35},
+        {"id": 4, "name": "Adam Smith", "age": 36}
+    ]
+
+    def resolve_user(self, info, user_id):
+        matched_users = [user for user in Query.users if user["id"] == user_id]
+        return matched_users[0] if matched_users else {"id": 4, "name": "Adam Smith", "age": 36}
 
 
 schema = Schema(query=Query)
 
 gql = '''
-{
-    hello(name: "graphQL")
+query {
+    user(userId: 2){
+        id
+        name
+        age
+    }
 }
 '''
 
