d5432bc84f6147c77fa86d0caafc0371c89dc94a
maheshwari <archit.maheshwari@payufin.com>
2023-12-17 17:46:23 +0530

    create user mutation

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-from graphene import Schema, ObjectType, String, Int, Field, List
+from graphene import Schema, ObjectType, String, Int, Field, List, Mutation
 
 
 class UserType(ObjectType):
     id = Int()
     name = String()
     age = Int()
+
+
+class CreateUser(Mutation):
+    class Arguments:
+        name = String()
+        age = Int()
+
+    user = Field(UserType)
+
+    def mutate(self, info, name, age):
+        user = {"id": len(Query.users)+1, "name": name, "age": age}
+        Query.users.append(user)
+        return CreateUser(user=user)
+
+
+class Mutation(ObjectType):
+    create_user = CreateUser.Field()
 
 
 class Query(ObjectType):
@@ -25,11 +42,25 @@ class Query(ObjectType):
     def resolve_users_by_min_age(self, info, min_age):
         return [user for user in Query.users if user["age"] >= min_age]
 
-schema = Schema(query=Query)
+schema = Schema(query=Query, mutation=Mutation)
 
-gql = '''
+
+gql_1 = '''
+mutation {
+    createUser(name:"Archit", age: 35){
+        user {
+            id
+            name
+            age
+        }
+    }
+}
+'''
+
+
+gql_2 = '''
 query {
-    usersByMinAge(minAge: 35){
+    user(userId: 5){
         id
         name
         age
@@ -37,6 +68,10 @@ query {
 }
 '''
 
+
+
 if __name__ == '__main__':
-    res = schema.execute(gql)
+    res = schema.execute(gql_1)
+    print(res)
+    res = schema.execute(gql_2)
     print(res)
