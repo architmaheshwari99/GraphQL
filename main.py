7d1199ba72868510d83b71d67c8a0f6cd71a058b
maheshwari <archit.maheshwari@payufin.com>
2023-12-17 23:37:10 +0530

    basic query

--- main.py
+++ main.py
@@ -1,10 +1,47 @@
-from graphene import Schema, ObjectType, String
+from graphene import Schema, ObjectType, String, Int, List, Field
 from fastapi import FastAPI
 from starlette_graphene3 import GraphQLApp, make_graphiql_handler, make_playground_handler
 
 
+class EmployerObject(ObjectType):
+    id = Int()
+    name = String()
+    contact_name = String()
+    industry = String()
+    jobs = List(lambda: JobObject)
+
+
+class JobObject(ObjectType):
+    id = Int()
+    title = String()
+    description = String()
+    employee_id = Int()
+    employer = Field(EmployerObject)
+
+employers_data = [
+    {"id": 1, "name": "MetaTechA", "contact_email": "contact@company-a.com", "industry": "Tech"},
+    {"id": 2, "name": "MoneySoftB", "contact_email": "contact@company-b.com", "industry": "Finance"},
+]
+
+jobs_data = [
+    {"id": 1, "title": "Software Engineer", "description": "Develop web applications", "employer_id": 1},
+    {"id": 2, "title": "Data Analyst", "description": "Analyze data and create reports", "employer_id": 1},
+    {"id": 3, "title": "Accountant", "description": "Manage financial records", "employer_id": 2},
+    {"id": 4, "title": "Manager", "description": "Manage people who manage records", "employer_id": 2},
+]
+
+
 class Query(ObjectType):
-    pass
+    jobs = List(JobObject)
+    employers = List(EmployerObject)
+
+    @staticmethod
+    def resolve_jobs(root, info):
+        return jobs_data
+
+    @staticmethod
+    def resolve_employers(root, info):
+        return employers_data
 
 
 schema = Schema(query=Query)
